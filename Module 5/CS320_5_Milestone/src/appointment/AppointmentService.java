2f5ac2ddb4f80ead1c8fdd5744b49a9c7106f5b0
DerekKwas <75740508+DerekKwas@users.noreply.github.com>
2023-09-30 17:04:59 -0400

    Split Appointment Object Creation & Adding To List
    Separated the job of the addAppointment method to instead have another method (createAppointment) deal with creating the Appointment object and the addAppointment method now deals with adding the object to the list.

--- Module 5/CS320_5_Milestone/src/appointment/AppointmentService.java
+++ Module 5/CS320_5_Milestone/src/appointment/AppointmentService.java
@@ -8,16 +8,19 @@ public class AppointmentService {
 	// Create array list object to hold list of appointments
 	public ArrayList<Appointment> appointmentList = new ArrayList<Appointment>(); 
 	
+	// Method to create appointment object
+	public Appointment createAppointment(String id, LocalDate date, String description) {
+		return new Appointment(id, date, description);
+	}
+	
 	// Function to add appointment to appointmentList
-	public void addAppointment(String id, LocalDate date, String description) {
+	public void addAppointment(Appointment appointment) {
 		for (int i = 0; i < appointmentList.size(); i++) {
-			if (appointmentList.get(i).getID() == id) {
+			if (appointmentList.get(i).getID() == appointment.getID()) {
 				throw new IllegalArgumentException("Appointment ID already exists");
 			}
 		}
-		Appointment appointment = new Appointment(id, date, description);
-		
-		appointmentList.add(appointment);
+			appointmentList.add(appointment);
 	}
 	
 	public void deleteAppointment(String id) {
