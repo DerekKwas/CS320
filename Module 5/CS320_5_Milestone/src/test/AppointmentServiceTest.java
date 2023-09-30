51dfef89568ba840f14a6b0dcc4cc65724799090
DerekKwas <75740508+DerekKwas@users.noreply.github.com>
2023-09-30 17:05:37 -0400

    Updated To Test New Methods

--- Module 5/CS320_5_Milestone/src/test/AppointmentServiceTest.java
+++ Module 5/CS320_5_Milestone/src/test/AppointmentServiceTest.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import appointment.Appointment;
 import appointment.AppointmentService;
 
 class AppointmentServiceTest {
@@ -14,24 +14,29 @@ class AppointmentServiceTest {
 	@Test
 	void testAppointmentServiceAddAppointment() {
 		AppointmentService appointmentService = new AppointmentService();
-		appointmentService.addAppointment("12345", LocalDate.of(2023, 10, 13), "Arrive at Appointment 1");
-		appointmentService.addAppointment("12346", LocalDate.of(2023, 10, 17), "Arrive at Appointment 2");
+		Appointment Appt1 = appointmentService.createAppointment("12345", LocalDate.of(2023, 10, 13), "Arrive at Appointment 1");
+		appointmentService.addAppointment(Appt1);
+		Appointment Appt2 = appointmentService.createAppointment("12346", LocalDate.of(2023, 10, 13), "Arrive at Appointment 2");
+		appointmentService.addAppointment(Appt2);
 	}
-		
+
 	@Test
 	void testAppointmentServiceAddAppointmentReoccuringId() {
 		AppointmentService appointmentService = new AppointmentService();
-		appointmentService.addAppointment("12345", LocalDate.of(2023, 10, 10), "Arrive at Appointment 1");
+		Appointment Appt1 = appointmentService.createAppointment("12345", LocalDate.of(2023, 10, 10), "Arrive at Appointment 1");
+		appointmentService.addAppointment(Appt1);
+		Appointment Appt2 = appointmentService.createAppointment("12345", LocalDate.of(2023, 10, 14), "Arrive at Appointment 2");
 		
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			appointmentService.addAppointment("12345", LocalDate.of(2023, 10, 14), "Arrive at Appointment 2");
+			appointmentService.addAppointment(Appt2);
 		});
 	}
 	
 	@Test
 	void testAppointmentServiceDeleteAppointmentId() {
 		AppointmentService appointmentService = new AppointmentService();
-		appointmentService.addAppointment("12345", LocalDate.of(2023, 10, 17), "Arrive at Appointment 1");
+		Appointment Appt1 = appointmentService.createAppointment("12345", LocalDate.of(2023, 10, 17), "Arrive at Appointment 1");
+		appointmentService.addAppointment(Appt1);
 		
 		appointmentService.deleteAppointment("12345");
 	}
@@ -39,7 +44,8 @@ class AppointmentServiceTest {
 	@Test
 	void testAppointmentServiceDeleteAppointmentInvalidId() {
 		AppointmentService appointmentService = new AppointmentService();
-		appointmentService.addAppointment("12345", LocalDate.of(2023, 11, 2), "Arrive at Appointment 1");
+		Appointment Appt1 = appointmentService.createAppointment("12345", LocalDate.of(2023, 11, 2), "Arrive at Appointment 1");
+		appointmentService.addAppointment(Appt1);
 		
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			appointmentService.deleteAppointment("15");
